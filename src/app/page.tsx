888f9ccb275f60aada0561951397e8f8bad2edad
Deepak Mane <108747825+Deepak-Mane1902@users.noreply.github.com>
2025-05-17 19:41:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function Home() {
-  redirect("/sign-in");
-}
+import FeaturesSection from "@/frontend/components/About";
+import PricingSection from "@/frontend/components/Cards";
+import Footer from "@/frontend/components/Footer";
+import Header from "@/frontend/components/Header";
+import HeroSection from "@/frontend/components/HeroSection";
+import Outro from "@/frontend/components/Outro";
+import TestimonialSlider from "@/frontend/components/Rating";
+import Views from "@/frontend/components/Views";
+import React from "react";// import wrapper
+
+const Page = () => {
+  return (
+      <main>
+        <Header />
+        <HeroSection />
+        <Views />
+        <FeaturesSection />
+        <PricingSection />
+        <TestimonialSlider />
+        <Outro />
+        <Footer />
+      </main>
+
+  );
+};
+
+export default Page;
